75c7b3e00e86a4210046f32ee4f9308d24860e3a
Paul Miller <paulmillr@me.com>
2011-09-05 03:21:11 +0300

    Added trailing space in terminal hostname prompts.

--- zshrc.sh
+++ zshrc.sh
@@ -31,7 +31,7 @@ source $ZSH/oh-my-zsh.sh
 
 # Theme.
 PRE='%{$fg_bold[red]%}➜'
-HOSTINFO="%{$fg_bold[yellow]%}%n@%m"
+HOSTINFO="%{$fg_bold[yellow]%} %n@%m"
 CURDIR="%{$fg_bold[green]%}%p %{$fg[cyan]%}%~ %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}"
 
 ZSH_THEME_GIT_PROMPT_PREFIX="git:(%{$fg[red]%}"
@@ -45,7 +45,7 @@ ISMYMAC="`uname -a | grep paulmillr`"
 if [[ -z "$ISMYMAC" ]]; then
   PROMPT="$PRE $HOSTINFO $CURDIR"
 else
-  PROMPT="$PRE $CURDIR"
+  PROMPT="$PRE $HOSTINFO $CURDIR"
 fi
 
 export PATH=$PATH:/usr/local/share/python:/usr/local/Cellar/python3/3.2.1/bin
