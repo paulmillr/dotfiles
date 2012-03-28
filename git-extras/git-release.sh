b1ce16bfc43703421b4562f5748467dde4e7fd38
Paul Miller <paul@paulmillr.com>
2012-03-28 22:30:05 +0300

    @{1}

---
+++ git-extras/git-release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+hook() {
+  local hook=".git/hooks/$1.sh"
+  if test -f $hook; then
+    echo "... $1"
+    . $hook
+  fi
+}
+
+if test $# -gt 0; then
+  hook pre-release
+  echo "... releasing $1"
+  git commit -a -m "Release $1." \
+    && git tag $1 \
+    && git push \
+    && git push --tags \
+    && hook post-release \
+    && echo "... complete"
+else
+  echo "tag required" 1>&2 && exit 1
+fi
