35a91abc54fce0be670c80b56da586986c8c2ce6
Paul Miller <paul@paulmillr.com>
2012-08-03 23:04:29 +0300

    Use better git scripts. Move ghpull to git-extras dir.

--- git-extras/git-add-upstream.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-git remote add upstream $1
--- git-extras/git-create-branch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-branch=$1
-test -z $branch && echo "branch required." 1>&2 && exit 1
-
-git push origin origin:refs/heads/$branch
-git fetch origin
-git checkout --track -b $branch origin/$branch
-git pull
