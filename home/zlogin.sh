354975b44d66c83b5914d0b77d600fb8924efcb0
Paul Miller <paul@paulmillr.com>
2012-09-26 15:08:17 +0300

    Remove prev authors.

--- home/zlogin.sh
+++ home/zlogin.sh
@@ -1,9 +1,6 @@
 #
 # Executes commands at login post-zshrc.
 #
-# Authors:
-#   Sorin Ionescu <sorin.ionescu@gmail.com>
-#
 
 # Execute code that does not affect the current session in the background.
 {
--- home/zprofile.sh
+++ home/zprofile.sh
@@ -1,7 +1,3 @@
 #
 # Executes commands at login pre-zshrc.
 #
-# Authors:
-#   Sorin Ionescu <sorin.ionescu@gmail.com>
-#
-
