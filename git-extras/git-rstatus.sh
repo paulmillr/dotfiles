f022f23a6264c49a4800e2018cb0f62620b040fe
Paul Miller <paul@paulmillr.com>
2012-10-27 06:40:31 +0800

    Add recursive git status.

---
+++ git-extras/git-rstatus.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+autoload colors && colors
+
+ls -1 | while read line; do
+  cd $line
+  line_count=$(git status --porcelain 2> /dev/null | wc -l)
+  if [[ "$line_count" -ne "0" ]]; then
+    echo "$fg[blue]$line$reset_color:"
+    git status -s
+    echo ""
+  fi
+  cd ..
+done
