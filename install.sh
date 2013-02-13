9cae23f1c0ff9af94fc7c9c2283f5b26b11a7ca5
Paul Miller <paul@paulmillr.com>
2013-02-13 22:28:55 +0200

    Supress warnings.

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 echo 'You might need to change your default shell to zsh: `chsh -s /bin/zsh` (or `sudo vim /etc/passwd`)'
 
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-
-$HOME}/.zprezto"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 dir="$HOME/Development/paulmillr/dotfiles"
 mkdir -p $dir
 cd $dir
 git clone git://github.com/paulmillr/dotfiles.git
 cd dotfiles
-sudo source bin/symlink-dotfiles.sh
+sudo bash bin/symlink-dotfiles.sh
