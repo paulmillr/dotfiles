5fe889ab6ba8fde39753b3aef6db009a43fc06a8
Paul Miller <paul@paulmillr.com>
2013-02-20 04:15:57 +0200

    List files on chdir.

--- git-extras/git-rstatus.sh
+++ git-extras/git-rstatus.sh
@@ -6,8 +6,8 @@ ls -1 | while read line; do
   cd $line
   line_count=$(git status --porcelain 2> /dev/null | wc -l)
   if [[ "$line_count" -ne "0" ]]; then
-    echo "$fg[blue]$line$reset_color:"
-    git status -s
+    echo "${fg[blue]}$line$reset_color:"
+    git status --short
     echo ""
   fi
   cd ..
