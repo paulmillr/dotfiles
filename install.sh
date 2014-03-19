097ff86b35627dced89c31b9f153ea2b4815c2b1
Paul Miller <paul@paulmillr.com>
2014-03-19 22:20:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo 'You might need to change your default shell to zsh: `chsh -s /bin/zsh` (or `sudo vim /etc/passwd`)'
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-dir="$HOME/Developer/paulmillr/dotfiles"
+dir="$HOME/Developer/paulmillr"
 mkdir -p $dir
 cd $dir
 git clone git://github.com/paulmillr/dotfiles.git
