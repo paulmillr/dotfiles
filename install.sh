2525588254c9060de5e10020ee918f61d6e9b0e2
Paul Miller <paul@paulmillr.com>
2014-07-24 20:12:56 +0300

    instructions

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ echo 'You might need to change your default shell to zsh: `chsh -s /bin/zsh` (or
 dir="$HOME/Developer/paulmillr"
 mkdir -p $dir
 cd $dir
-git clone git://github.com/paulmillr/dotfiles.git
+git clone --recursive git://github.com/paulmillr/dotfiles.git
 cd dotfiles
 sudo bash symlink-dotfiles.sh
