3254bca47ff46fd0194921e9c1842c98e19487c0
Paul Miller <paul@paulmillr.com>
2014-07-24 20:10:49 +0300

    Throw away zprezto. Faster, simpler.

---
+++ terminal/startup.sh
@@ -0,0 +1,548 @@
+#
+# Initializes Prezto.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+
+# Set case-sensitivity for completion, history lookup, etc.
+zstyle ':prezto:*:*' case-sensitive 'no'
+
+# Color output (auto set to 'no' on dumb terminals).
+zstyle ':prezto:*:*' color 'yes'
+
+# Auto set the tab and window titles.
+zstyle ':prezto:module:terminal' auto-title 'yes'
+
+#
+# Sets directory options and defines directory aliases.
+#
+# Authors:
+#   James Cox <james@imaj.es>
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+#
+# Options
+#
+
+setopt AUTO_CD              # Auto changes to a directory without typing cd.
+setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
+setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
+setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
+setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
+setopt CDABLE_VARS          # Change directory to a path stored in a variable.
+setopt AUTO_NAME_DIRS       # Auto add variable-stored paths to ~ list.
+setopt MULTIOS              # Write to multiple descriptors.
+setopt EXTENDED_GLOB        # Use extended globbing syntax.
+unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
+                            # Use >! and >>! to bypass.
+
+#
+# Aliases
+#
+
+alias d='dirs -v'
+for index ({1..9}) alias "$index"="cd +${index}"; unset index
+
+#
+# Sets general shell options and defines environment variables.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+#
+# Smart URLs
+#
+
+autoload -Uz url-quote-magic
+zle -N self-insert url-quote-magic
+
+#
+# General
+#
+
+setopt BRACE_CCL          # Allow brace character class list expansion.
+setopt COMBINING_CHARS    # Combine zero-length punctuation characters (accents)
+                          # with the base character.
+setopt RC_QUOTES          # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
+unsetopt MAIL_WARNING     # Don't print a warning message if a mail file has been accessed.
+
+#
+# Jobs
+#
+
+setopt LONG_LIST_JOBS     # List jobs in the long format by default.
+setopt AUTO_RESUME        # Attempt to resume existing job before creating a new process.
+setopt NOTIFY             # Report status of background jobs immediately.
+unsetopt BG_NICE          # Don't run all background jobs at a lower priority.
+unsetopt HUP              # Don't kill jobs on shell exit.
+unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
+
+#
+# Grep
+#
+
+if zstyle -t ':prezto:environment:grep' color; then
+  export GREP_COLOR='37;45'
+  export GREP_OPTIONS='--color=auto'
+fi
+
+#
+# Termcap
+#
+
+if zstyle -t ':prezto:environment:termcap' color; then
+  export LESS_TERMCAP_mb=$'\E[01;31m'      # Begins blinking.
+  export LESS_TERMCAP_md=$'\E[01;31m'      # Begins bold.
+  export LESS_TERMCAP_me=$'\E[0m'          # Ends mode.
+  export LESS_TERMCAP_se=$'\E[0m'          # Ends standout-mode.
+  export LESS_TERMCAP_so=$'\E[00;47;30m'   # Begins standout-mode.
+  export LESS_TERMCAP_ue=$'\E[0m'          # Ends underline.
+  export LESS_TERMCAP_us=$'\E[01;32m'      # Begins underline.
+fi
+
+
+
+
+#
+# Sets history options and defines history aliases.
+#
+# Authors:
+#   Robby Russell <robby@planetargon.com>
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+#
+# Variables
+#
+
+HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"       # The path to the history file.
+HISTSIZE=10000                   # The maximum number of events to save in the internal history.
+SAVEHIST=10000                   # The maximum number of events to save in the history file.
+
+#
+# Options
+#
+
+setopt BANG_HIST                 # Treat the '!' character specially during expansion.
+setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
+setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
+setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
+setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
+setopt HIST_BEEP                 # Beep when accessing non-existent history.
+
+#
+# Aliases
+#
+
+# Lists the ten most used commands.
+alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
+
+
+#
+# Provides for easier use of 256 colors and effects.
+#
+# Authors:
+#   P.C. Shyamshankar <sykora@lucentbeing.com>
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Return if requirements are not found.
+if [[ "$TERM" == 'dumb' ]]; then
+  return 1
+fi
+
+typeset -gA FX FG BG
+
+FX=(
+                                        none                         "\e[00m"
+                                        normal                       "\e[22m"
+  bold                      "\e[01m"    no-bold                      "\e[22m"
+  faint                     "\e[02m"    no-faint                     "\e[22m"
+  standout                  "\e[03m"    no-standout                  "\e[23m"
+  underline                 "\e[04m"    no-underline                 "\e[24m"
+  blink                     "\e[05m"    no-blink                     "\e[25m"
+  fast-blink                "\e[06m"    no-fast-blink                "\e[25m"
+  reverse                   "\e[07m"    no-reverse                   "\e[27m"
+  conceal                   "\e[08m"    no-conceal                   "\e[28m"
+  strikethrough             "\e[09m"    no-strikethrough             "\e[29m"
+  gothic                    "\e[20m"    no-gothic                    "\e[22m"
+  double-underline          "\e[21m"    no-double-underline          "\e[22m"
+  proportional              "\e[26m"    no-proportional              "\e[50m"
+  overline                  "\e[53m"    no-overline                  "\e[55m"
+
+                                        no-border                    "\e[54m"
+  border-rectangle          "\e[51m"    no-border-rectangle          "\e[54m"
+  border-circle             "\e[52m"    no-border-circle             "\e[54m"
+
+                                        no-ideogram-marking          "\e[65m"
+  underline-or-right        "\e[60m"    no-underline-or-right        "\e[65m"
+  double-underline-or-right "\e[61m"    no-double-underline-or-right "\e[65m"
+  overline-or-left          "\e[62m"    no-overline-or-left          "\e[65m"
+  double-overline-or-left   "\e[63m"    no-double-overline-or-left   "\e[65m"
+  stress                    "\e[64m"    no-stress                    "\e[65m"
+
+                                        font-default                 "\e[10m"
+  font-first                "\e[11m"    no-font-first                "\e[10m"
+  font-second               "\e[12m"    no-font-second               "\e[10m"
+  font-third                "\e[13m"    no-font-third                "\e[10m"
+  font-fourth               "\e[14m"    no-font-fourth               "\e[10m"
+  font-fifth                "\e[15m"    no-font-fifth                "\e[10m"
+  font-sixth                "\e[16m"    no-font-sixth                "\e[10m"
+  font-seventh              "\e[17m"    no-font-seventh              "\e[10m"
+  font-eigth                "\e[18m"    no-font-eigth                "\e[10m"
+  font-ninth                "\e[19m"    no-font-ninth                "\e[10m"
+)
+
+FG[none]="$FX[none]"
+BG[none]="$FX[none]"
+colors=(black red green yellow blue magenta cyan white)
+for color in {0..255}; do
+  if (( $color >= 0 )) && (( $color < $#colors )); then
+    index=$(( $color + 1 ))
+    FG[$colors[$index]]="\e[38;5;${color}m"
+    BG[$colors[$index]]="\e[48;5;${color}m"
+  fi
+
+  FG[$color]="\e[38;5;${color}m"
+  BG[$color]="\e[48;5;${color}m"
+done
+unset color{s,} index
+
+
+#
+# Sets terminal window and tab titles.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Return if requirements are not found.
+if [[ "$TERM" == (dumb|linux|*bsd*) ]]; then
+  return 1
+fi
+
+# Sets the terminal or terminal multiplexer window title.
+function set-window-title {
+  local title_format{,ted}
+  zstyle -s ':prezto:module:terminal:window-title' format 'title_format' || title_format="%s"
+  zformat -f title_formatted "$title_format" "s:$argv"
+
+  if [[ "$TERM" == screen* ]]; then
+    title_format="\ek%s\e\\"
+  else
+    title_format="\e]2;%s\a"
+  fi
+
+  printf "$title_format" "${(V%)title_formatted}"
+}
+
+# Sets the terminal tab title.
+function set-tab-title {
+  local title_format{,ted}
+  zstyle -s ':prezto:module:terminal:tab-title' format 'title_format' || title_format="%s"
+  zformat -f title_formatted "$title_format" "s:$argv"
+
+  printf "\e]1;%s\a" ${(V%)title_formatted}
+}
+
+# Sets the tab and window titles with a given command.
+function _terminal-set-titles-with-command {
+  emulate -L zsh
+  setopt EXTENDED_GLOB
+
+  # Get the command name that is under job control.
+  if [[ "${2[(w)1]}" == (fg|%*)(\;|) ]]; then
+    # Get the job name, and, if missing, set it to the default %+.
+    local job_name="${${2[(wr)%*(\;|)]}:-%+}"
+
+    # Make a local copy for use in the subshell.
+    local -A jobtexts_from_parent_shell
+    jobtexts_from_parent_shell=(${(kv)jobtexts})
+
+    jobs "$job_name" 2>/dev/null > >(
+      read index discarded
+      # The index is already surrounded by brackets: [1].
+      _terminal-set-titles-with-command "${(e):-\$jobtexts_from_parent_shell$index}"
+    )
+  else
+    # Set the command name, or in the case of sudo or ssh, the next command.
+    local cmd="${${2[(wr)^(*=*|sudo|ssh|-*)]}:t}"
+    local truncated_cmd="${cmd/(#m)?(#c15,)/${MATCH[1,12]}...}"
+    unset MATCH
+
+    set-window-title "$cmd"
+    set-tab-title "$truncated_cmd"
+  fi
+}
+
+# Sets the tab and window titles with a given path.
+function _terminal-set-titles-with-path {
+  emulate -L zsh
+  setopt EXTENDED_GLOB
+
+  local absolute_path="${${1:a}:-$PWD}"
+  local abbreviated_path="${absolute_path/#$HOME/~}"
+  local truncated_path="${abbreviated_path/(#m)?(#c15,)/...${MATCH[-12,-1]}}"
+  unset MATCH
+
+  set-window-title "$abbreviated_path"
+  set-tab-title "$truncated_path"
+}
+
+# Sets the Terminal.app proxy icon.
+function _terminal-set-terminal-app-proxy-icon {
+  printf '\e]7;%s\a' "file://$HOST${${1:-$PWD}// /%20}"
+}
+
+# Do not override precmd/preexec; append to the hook array.
+autoload -Uz add-zsh-hook
+
+# Set up the Apple Terminal.
+if [[ "$TERM_PROGRAM" == 'Apple_Terminal' ]] \
+  && ( ! [[ -n "$STY" || -n "$TMUX" || -n "$DVTM" ]] )
+then
+  # Sets the Terminal.app current working directory before the prompt is
+  # displayed.
+  add-zsh-hook precmd _terminal-set-terminal-app-proxy-icon
+
+  # Unsets the Terminal.app current working directory when a terminal
+  # multiplexer or remote connection is started since it can no longer be
+  # updated, and it becomes confusing when the directory displayed in the title
+  # bar is no longer synchronized with real current working directory.
+  function _terminal-unset-terminal-app-proxy-icon {
+    if [[ "${2[(w)1]:t}" == (screen|tmux|dvtm|ssh|mosh) ]]; then
+      _terminal-set-terminal-app-proxy-icon ' '
+    fi
+  }
+  add-zsh-hook preexec _terminal-unset-terminal-app-proxy-icon
+
+  # Do not set the tab and window titles in Terminal.app since it sets the tab
+  # title to the currently running process by default and the current working
+  # directory is set separately.
+  return
+fi
+
+# Set up non-Apple terminals.
+if zstyle -t ':prezto:module:terminal' auto-title \
+  && ( ! [[ -n "$STY" || -n "$TMUX" ]] )
+then
+  # Sets the tab and window titles before the prompt is displayed.
+  add-zsh-hook precmd _terminal-set-titles-with-path
+
+  # Sets the tab and window titles before command execution.
+  add-zsh-hook preexec _terminal-set-titles-with-command
+fi
+
+
+#
+# Defines general aliases and functions.
+#
+# Authors:
+#   Robby Russell <robby@planetargon.com>
+#   Suraj N. Kurapati <sunaku@gmail.com>
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Checks if a name is a command, function, or alias.
+function is-callable {
+  (( $+commands[$1] )) || (( $+functions[$1] )) || (( $+aliases[$1] ))
+}
+
+# Correct commands.
+setopt CORRECT
+
+#
+# Aliases
+#
+
+# Disable correction.
+alias ack='nocorrect ack'
+alias cd='nocorrect cd'
+alias cp='nocorrect cp'
+alias ebuild='nocorrect ebuild'
+alias gcc='nocorrect gcc'
+alias gist='nocorrect gist'
+alias grep='nocorrect grep'
+alias heroku='nocorrect heroku'
+alias ln='nocorrect ln'
+alias man='nocorrect man'
+alias mkdir='nocorrect mkdir'
+alias mv='nocorrect mv'
+alias mysql='nocorrect mysql'
+alias rm='nocorrect rm'
+
+# Disable globbing.
+alias bower='noglob bower'
+alias fc='noglob fc'
+alias find='noglob find'
+alias ftp='noglob ftp'
+alias history='noglob history'
+alias locate='noglob locate'
+alias rake='noglob rake'
+alias rsync='noglob rsync'
+alias scp='noglob scp'
+alias sftp='noglob sftp'
+
+# Define general aliases.
+alias _='sudo'
+alias b='${(z)BROWSER}'
+alias cp="${aliases[cp]:-cp} -i"
+alias e='${(z)VISUAL:-${(z)EDITOR}}'
+alias ln="${aliases[ln]:-ln} -i"
+alias mkdir="${aliases[mkdir]:-mkdir} -p"
+alias mv="${aliases[mv]:-mv} -i"
+alias p='${(z)PAGER}'
+alias po='popd'
+alias pu='pushd'
+alias rm="${aliases[rm]:-rm} -i"
+alias type='type -a'
+
+# ls
+if is-callable 'dircolors'; then
+  # GNU Core Utilities
+  alias ls='ls --group-directories-first'
+
+  if zstyle -t ':prezto:module:utility:ls' color; then
+    if [[ -s "$HOME/.dir_colors" ]]; then
+      eval "$(dircolors "$HOME/.dir_colors")"
+    else
+      eval "$(dircolors)"
+    fi
+
+    alias ls="$aliases[ls] --color=auto"
+  else
+    alias ls="$aliases[ls] -F"
+  fi
+else
+  # BSD Core Utilities
+  if zstyle -t ':prezto:module:utility:ls' color; then
+    # Define colors for BSD ls.
+    export LSCOLORS='exfxcxdxbxGxDxabagacad'
+
+    # Define colors for the completion system.
+    export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
+
+    alias ls='ls -G'
+  else
+    alias ls='ls -F'
+  fi
+fi
+
+alias l='ls -1A'         # Lists in one column, hidden files.
+alias ll='ls -lh'        # Lists human readable sizes.
+alias lr='ll -R'         # Lists human readable sizes, recursively.
+alias la='ll -A'         # Lists human readable sizes, hidden files.
+alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
+alias lx='ll -XB'        # Lists sorted by extension (GNU only).
+alias lk='ll -Sr'        # Lists sorted by size, largest last.
+alias lt='ll -tr'        # Lists sorted by date, most recent last.
+alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
+alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
+alias sl='ls'            # I often screw this up.
+
+# Mac OS X Everywhere
+if [[ "$OSTYPE" == darwin* ]]; then
+  alias o='open'
+elif [[ "$OSTYPE" == cygwin* ]]; then
+  alias o='cygstart'
+  alias pbcopy='tee > /dev/clipboard'
+  alias pbpaste='cat /dev/clipboard'
+else
+  alias o='xdg-open'
+
+  if (( $+commands[xclip] )); then
+    alias pbcopy='xclip -selection clipboard -in'
+    alias pbpaste='xclip -selection clipboard -out'
+  elif (( $+commands[xsel] )); then
+    alias pbcopy='xsel --clipboard --input'
+    alias pbpaste='xsel --clipboard --output'
+  fi
+fi
+
+alias pbc='pbcopy'
+alias pbp='pbpaste'
+
+# File Download
+if (( $+commands[curl] )); then
+  alias get='curl --continue-at - --location --progress-bar --remote-name --remote-time'
+elif (( $+commands[wget] )); then
+  alias get='wget --continue --progress=bar --timestamping'
+fi
+
+# Resource Usage
+alias df='df -kh'
+alias du='du -kh'
+
+if (( $+commands[htop] )); then
+  alias top=htop
+else
+  alias topc='top -o cpu'
+  alias topm='top -o vsize'
+fi
+
+# Miscellaneous
+
+# Serves a directory via HTTP.
+alias http-serve='python -m SimpleHTTPServer'
+
+#
+# Functions
+#
+
+# Makes a directory and changes to it.
+function mkdcd {
+  [[ -n "$1" ]] && mkdir -p "$1" && builtin cd "$1"
+}
+
+# Changes to a directory and lists its contents.
+function cdls {
+  builtin cd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Pushes an entry onto the directory stack and lists its contents.
+function pushdls {
+  builtin pushd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Pops an entry off the directory stack and lists its contents.
+function popdls {
+  builtin popd "$argv[-1]" && ls "${(@)argv[1,-2]}"
+}
+
+# Prints columns 1 2 3 ... n.
+function slit {
+  awk "{ print ${(j:,:):-\$${^@}} }"
+}
+
+# Finds files and executes a command on them.
+function find-exec {
+  find . -type f -iname "*${1:-}*" -exec "${2:-file}" '{}' \;
+}
+
+# Displays user owned processes status.
+function psu {
+  ps -U "${1:-$USER}" -o 'pid,%cpu,%mem,command' "${(@)argv[2,-1]}"
+}
+
+
+function diff {
+  if zstyle -t ':prezto:module:utility:diff' color; then
+    if (( $+commands[colordiff] )); then
+      command diff --unified "$@" | colordiff --difftype diffu
+    elif (( $+commands[git] )); then
+      git --no-pager diff --color=auto --no-ext-diff --no-index "$@"
+    else
+      command diff --unified "$@"
+    fi
+  else
+    command diff --unified "$@"
+  fi
+}
