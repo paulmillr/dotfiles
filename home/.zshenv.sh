e3933fd9c213704c4084d08c4aecbeedb92a10e7
Paul Miller <paul@paulmillr.com>
2014-11-21 11:33:51 +0200

    Remove ADB

--- home/.zshenv.sh
+++ home/.zshenv.sh
@@ -57,8 +57,6 @@ com="$dev/com"
 pm="$dev/paulmillr"
 as="$HOME/Library/Application Support"
 
-export ANDROID_HOME="/usr/local/Cellar/android-sdk/22.6"
-
 # Set the the list of directories that cd searches.
 cdpath=(
   $cdpath
@@ -83,8 +81,6 @@ for path_file in /etc/manpaths.d/*(.N); do
 done
 unset path_file
 
-alias adb=$ANDROID_HOME/platform-tools/adb
-
 # Set the list of directories that Zsh searches for programs.
 # export PYTHONPATH=/usr/local/lib/python2.7/site-packages
 path=(
@@ -94,7 +90,6 @@ path=(
   /usr/local/share/npm/bin
   /usr/{bin,sbin}
   /{bin,sbin}
-  $ANDROID_HOME/tools
   $path
 )
 
