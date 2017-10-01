7d484f349763b9615575fa3198a1ae1a60016c6b
Paul Miller <paul@paulmillr.com>
2017-10-01 03:18:43 +0300

    Update clone path to Developer/personal.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo 'You might need to change your default shell to zsh: `chsh -s /bin/zsh` (or `sudo vim /etc/passwd`)'
 
-dir="$HOME/Developer/paulmillr"
+dir="$HOME/Developer/personal"
 mkdir -p $dir
 cd $dir
 git clone --recursive git://github.com/paulmillr/dotfiles.git
